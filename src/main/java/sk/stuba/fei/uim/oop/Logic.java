98a2a308125d0eafd871e2bf37866ae5f5b74f69
ViktorHruz <xhruzv@stuba.sk>
2022-04-06 19:00:32 +0200

    NEfunguje zmenenie focusu pri manipulacii zo sliderom

---
+++ src/main/java/sk/stuba/fei/uim/oop/Logic.java
@@ -0,0 +1,39 @@
+package sk.stuba.fei.uim.oop;
+
+import lombok.Getter;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Logic implements KeyListener, ChangeListener {
+
+    @Getter
+    private JLabel label;
+
+    public Logic() {
+        this.label = new JLabel("Zaciatocny text");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Stlacil som: " + e.getKeyChar());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        this.label.setText("Cislo: " + ((JSlider) e.getSource()).getValue());
+    }
+}
--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,37 @@
 package sk.stuba.fei.uim.oop;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        JFrame frame =  new JFrame("okno");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.CYAN);
+        frame.setLayout(new BorderLayout());
+        frame.setFocusable(true);
+
+        Logic logic = new Logic();
+        frame.addKeyListener(logic);
+
+        JPanel menu = new JPanel();
+        menu.setBackground(Color.YELLOW);
+        menu.setLayout(new GridLayout(2, 1));
+
+
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 10, 50, 20);
+        slider.setMajorTickSpacing(5);
+        slider.setMinorTickSpacing(5);
+        slider.setSnapToTicks(true);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.addChangeListener(logic);
+
+        menu.add(slider);
+        menu.add(logic.getLabel());
+        frame.add(menu, BorderLayout.PAGE_END);
+        frame.setVisible(true);
     }
 }
