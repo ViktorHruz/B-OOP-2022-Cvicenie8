7eb4407b2edc759ec8ded3b25c20c134c159a37c
ViktorHruz <xhruzv@stuba.sk>
2022-04-07 14:43:21 +0200

    more generic types normalne hashmapy

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,17 +2,20 @@ package sk.stuba.fei.uim.oop;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Message> messages = new ArrayList<>();
 
-        messages.add(new Message("hello"));
-        messages.add(new Message("world"));
-
-        for(Message m: messages){
-            System.out.println(m.getContent());
-        }
+        ConcreteIndex concreteIndex = new ConcreteIndex();
+        concreteIndex.add(new Message("hello"));
+        concreteIndex.add(new Message("world"));
+        concreteIndex.add(new Message("hello world"));
+        System.out.println(concreteIndex.toString());
     }
+
+
 }
